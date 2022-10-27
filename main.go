13025fbdab6b14b6ac338800b610c612493a3557
mnarvaezm96 <jmnarvaez47@misena.edu.co>
2022-10-27 09:28:25 -0500

    Usuarios

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/mnarvaezm96/doom_go/db"
 	"github.com/mnarvaezm96/doom_go/handlers"
 )
 
 func main() {
 
-	if bd.ChequeoConnection() == 0 {
+	if db.ChequeoConnection() == 0 {
 		log.Fatal("Sin conexion a la BD")
 
 	}
